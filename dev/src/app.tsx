f7c8da0f675ee94d92df3bbf3a80f63ab3cb9576
aidanaden <aidan@u.nus.edu>
2024-01-13 04:36:47 +0800

    chore: use solid-glow package in demo

--- dev/src/app.tsx
+++ dev/src/app.tsx
@@ -1,22 +1,20 @@
+import { Glow, GlowCapture } from "solid-glow";
 import type { Component } from "solid-js";
 
-import { Glow, GlowCapture } from "../../src";
-import styles from "./app.module.css";
+import "./main.css";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <div>
-        GLOW CAPTURE:
-        <GlowCapture>
-          <span>This won't glow</span>
-          <Glow color="hsl(338.69 100% 48.04%)">
-            <span class="glowable-text">
-              This will glow pink when the mouse is passed over
-            </span>
-          </Glow>
-        </GlowCapture>
-      </div>
+    <div>
+      GLOW CAPTURE:
+      <GlowCapture>
+        <span>This won't glow</span>
+        <Glow color="hsl(338.69 100% 48.04%)">
+          <span class="glowable-text">
+            This will glow pink when the mouse is passed over
+          </span>
+        </Glow>
+      </GlowCapture>
     </div>
   );
 };
